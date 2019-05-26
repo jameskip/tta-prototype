f1fc17175d0d8d8314f0842b04955f6023eebfc1
jkcon <github@xiixii.pw>
2019-05-26 16:23:02 -0500

    Convert TreeGrid to hook

--- src/Components/TreeGrid.jsx
+++ src/Components/TreeGrid.jsx
@@ -1,38 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import MediaCard from './MediaCard'
 
-export default class ApprovalGrid extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      trees: [],
-      loaded: false
-    }
-  }
-  componentDidMount () {
-    fetch(`http://localhost:3000/Trees?filter[order][0]=timeUpdated%20desc&filter[limit][1]=250&filter[skip]=5000`)
+const ApprovalGrid = () => {
+  const [trees, setTrees] = React.useState([])
+  const [loaded, setLoaded] = React.useState(false)
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/Trees?filter[order][0]=timeUpdated%20desc&filter[limit][1]=100&filter[skip]=5000`)
       .then((response) => {
         return response.json()
       })
       .then((myJson) => {
-        this.setState({ trees: myJson, loaded: true })
+        setTrees(myJson)
+        setLoaded(true)
       })
-  }
+  })
 
-  render () {
-    if (!this.state.loaded) {
-      return (
-        <div>
-          <LinearProgress color="secondary" variant="query" />
-        </div>
-      )
-    } else {
-      return (
-        <div style={{ marginLeft: 45, marginTop: 30 }}>
-          <MediaCard style={{ alignContent: 'center' }} trees={this.state.trees}/>
-        </div>
-      )
-    }
+  if (!loaded) {
+    return (
+      <div>
+        <LinearProgress color="secondary" variant="query" />
+      </div>
+    )
+  } else {
+    return (
+      <div style={{ marginLeft: 45, marginTop: 30 }}>
+        <MediaCard style={{ alignContent: 'center' }} trees={trees}/>
+      </div>
+    )
   }
 }
